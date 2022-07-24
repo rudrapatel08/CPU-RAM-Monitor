93d94419ecd4d5ed681ff9da7ff18a0db734f9d9
Rudra Patel <109606873+rudrapatel08@users.noreply.github.com>
2022-07-24 22:11:07 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+import psutil
+
+def display_usage(cpu_usage, mem_usage, bars=50):
+    cpu_percent = (cpu_usage / 100.0)
+    cpu_bar = '█' * int(cpu_percent * bars) + '-' * (bars - int(cpu_percent * bars))
+
+    mem_percent = (mem_usage / 100.0)
+    mem_bar = '█' * int(mem_percent * bars) + '-' * (bars - int(mem_percent * bars))
+
+    print(f"\rCPU Usage : |{cpu_bar}| {cpu_usage:.2f}%  ", end="")
+    print(f"MEM Usage: |{mem_bar}| {mem_usage:.2f}%  ",end="\r")
+
+
+while True:
+    display_usage(psutil.cpu_percent(), psutil.virtual_memory().percent, 30)
+    time.sleep(0.5)
+
+# █ special character to represet the bars
+# to run in the terminal locate the directory the code is stores in then type: python main.py
